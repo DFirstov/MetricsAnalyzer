8d4ada2be59c6c082574ab95f1edb6fc4d9c358c
Фирстов Дмитрий <FirstovDI@antereal.com>
2026-03-14 22:16:58 +0700

    Add Swagger

--- VictimService/Program.cs
+++ VictimService/Program.cs
@@ -1,7 +1,14 @@
 using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpMetrics();
 
