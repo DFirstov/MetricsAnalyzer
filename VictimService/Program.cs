15a94f092cbe4e2c4357883eb8b9551d4dd90e09
Фирстов Дмитрий <FirstovDI@antereal.com>
2026-03-14 22:02:16 +0700

    Add Prometheus to the Victim service

--- VictimService/Program.cs
+++ VictimService/Program.cs
@@ -1,6 +1,12 @@
+using Prometheus;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseHttpMetrics();
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapMetrics();
 
 app.Run();
